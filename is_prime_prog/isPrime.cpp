4bdea8700a783fb98158c9fa28863b9a14907d5b
Chehyde <quentin.crosnier@gmail.com>
2022-09-25 20:14:11 +0200

    oui

---
+++ is_prime_prog/isPrime.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "isPrime.h"
+
+int n = 1;
+bool isPrime(int n) {
+    if (n == 1 || n % 2 == 0) {
+        return false;
+    }
+    for (int i = 3; i <= std::sqrt(n); i += 2){
+        if (n % i == 0){
+            return false;
+        }
+    }
+    return true;
+}
---
+++ is_prime_prog/isPrime.h
@@ -0,0 +1,4 @@
+#pragma once
+
+extern int n;
+bool isPrime(int n);
