542117c3748163316e98ff92f39d4a0cf4f6b86c
Chehyde <quentin.crosnier@gmail.com>
2022-09-25 20:15:58 +0200

    isPrime fini

---
+++ is_prime_prog/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "isPrime.h"
+
+void main() {
+    int n;
+    while(std::cin >> n){
+        if(isPrime(n)){
+            std::cout << n << " is a prime: True\n";
+        }
+        else{
+            std::cout << n << " is a prime: False\n";
+        }
+    }
+    return;
+}
