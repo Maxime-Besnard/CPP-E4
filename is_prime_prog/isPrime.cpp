53ba09d67ccd476839cbd2ef680253dd5d427e17
Quentin Crosnier <105451831+Chehyde@users.noreply.github.com>
2022-10-05 09:55:20 +0200

    Update isPrime.cpp

--- is_prime_prog/isPrime.cpp
+++ is_prime_prog/isPrime.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "isPrime.h"
+#include <cmath>
 
 int n = 1;
 bool isPrime(int64_t n) {
@@ -9,7 +10,7 @@ bool isPrime(int64_t n) {
     if (n == 1 || n % 2 == 0) {
         return false;
     }
-    for (int i = 3; i <= std::sqrt(n); i += 2){
+    for (int i = 3; i <= sqrt(n); i += 2){
         if (n % i == 0){
             return false;
         }
