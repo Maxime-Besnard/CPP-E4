7efc9be209732aeca80bc37a525b15da879f7f45
Quentin Crosnier <105451831+Chehyde@users.noreply.github.com>
2022-10-05 08:24:23 +0200

    Update main.cpp

--- is_prime_prog/main.cpp
+++ is_prime_prog/main.cpp
@@ -3,7 +3,8 @@
 
 void main() {
     int n;
-    while(std::cin >> n){
+    while(std::cin.peek() != '\n'){
+	    std::cin >> n;
         if(isPrime(n)){
             std::cout << n << " is a prime: True\n";
         }
